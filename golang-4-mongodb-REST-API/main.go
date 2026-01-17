e1d90d415d19886ef4348ffeb9acd175df85d625
Abdullah Al Zubaer <zubaerislam703@gmail.com>
2026-01-18 01:23:53 +0600

    updated-mongodb-connection

--- golang-4-mongodb-REST-API/main.go
+++ golang-4-mongodb-REST-API/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
+	"github.com/abdullahalzubaerofficial/mongo-golang/controllers"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
-	"net/http"
 )
 
-func main(){
-	
+func main() {
+	r := httprouter.New()
+
+	session := getSession()
+	defer session.Close()
+
+	uc := controllers.NewUserController(session)
+
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+
+	http.ListenAndServe(":9000", r)
+}
+
+func getSession() *mgo.Session {
+    s, err := mgo.DialWithTimeout("mongodb+srv://username:password@cluster0.rapspdi.mongodb.net/?appName=Cluster0", 10*time.Second)
+    if err != nil {
+        panic(err)  
+    }
+    s.SetMode(mgo.Monotonic, true)
+    return s
 }
 
